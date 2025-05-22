c8a261c11ec633df4ed1702524aa1fee100d30fb
lola adriana <adryland79@gmail.com>
2025-05-22 15:06:20 +0200

    Add S3 bucket config and .envsecrets file

---
+++ infrastructure/s3.tf
@@ -0,0 +1,28 @@
+resource "aws_s3_bucket" "avatars" {
+  bucket = "grocerymate-avatarsadry"  #
+  acl    = "private" #acceso privado
+
+  tags = {
+    Name        = "grocerymate-avatars"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.avatars.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "avatars_block" {
+  bucket = aws_s3_bucket.avatars.id
+
+  block_public_acls       = true  #bucket privado
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
